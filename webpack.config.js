6876ff09dc935c30d30cfc6e6b1dcda0f1feb471
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-01 16:01:12 +1300

    Set up webpack link to CSS

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,16 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    path: path.resolve(__dirname, 'dist')
+    //clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   devtool: 'inline-source-map',
 };
