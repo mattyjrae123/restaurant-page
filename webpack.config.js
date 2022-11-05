08d2a8b1824d450f95678f85f82de720ca0a8b81
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-05 14:36:32 +1300

    Set up webpack html-loader and html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/scripts/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-    //clean: true
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html'
+    }),
+  ],
   module: {
     rules: [
       {
@@ -17,6 +24,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
