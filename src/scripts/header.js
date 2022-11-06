3c8b12e0e583aca46b43f11ca896a19def491339
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 16:06:17 +1300

    Generate img with js

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,7 +1,19 @@
+import logo from '../images/logo-md.png';
+
 /**
  * Generates all html content for page header, and appends it to the
  * div#content element
  */
 export default function generateHeader() {
-  console.log('GENERATED HEADER');
+  const header = document.createElement('header');
+
+  const img = document.createElement('img');
+  img.setAttribute('id', 'logo');
+  img.setAttribute('src', logo);
+  img.setAttribute('alt', 'Burger logo');
+  img.setAttribute('height', '100');
+  img.setAttribute('width', '193');
+
+  header.appendChild(img);
+  document.querySelector('div#content').appendChild(header);
 }
