c596eefbf374db149bb30b1904a970f0e46a4514
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 18:04:42 +1300

    Add first line of contact details to generate

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -5,5 +5,25 @@
   const contactDiv = document.createElement('div');
   contactDiv.setAttribute('id', 'contact');
 
+  // Heading
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Contact Us';
+
+  // Contact list
+  const ul = document.createElement('ul');
+  
+  const address = document.createElement('li');
+  const addressSpan1 = document.createElement('span');
+  addressSpan1.textContent = 'Address';
+  const addressSpan2 = document.createElement('span');
+  addressSpan2.textContent = '324 Big Fisher Ave';
+
+  address.appendChild(addressSpan1);
+  address.appendChild(addressSpan2);
+
+  ul.appendChild(address);
+
+  contactDiv.appendChild(h1);
+  contactDiv.appendChild(ul);
   return contactDiv;
 })();
