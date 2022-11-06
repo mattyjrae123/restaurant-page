f312eb562ade7fe73dd50ecf96c2a50338e6e0c5
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 17:55:56 +1300

    Create skeleton for contact page generator

---
+++ src/scripts/contact.js
@@ -0,0 +1,9 @@
+/**
+ * Generates all html content for contact page, and returns it as an object
+ */
+ export default (() => {
+  const contactDiv = document.createElement('div');
+  contactDiv.setAttribute('id', 'contact');
+
+  return contactDiv;
+})();
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,11 @@
 import header from './header.js'
 import home from './home.js';
 import menu from './menu.js';
+import contact from './contact';
 import '../styles/style.css';
 
 const main = document.createElement('main');
-main.appendChild(menu);
+main.appendChild(contact);
 
 document.querySelector('div#content').appendChild(header);
 document.querySelector('div#content').appendChild(main);
@@ -16,7 +17,7 @@ document.querySelector('button[data-page="menu"]')
         .addEventListener('click', () => navBtnListener(menu));
 
 document.querySelector('button[data-page="contact"]')
-        .addEventListener('click', () => navBtnListener(document.createElement('div')));
+        .addEventListener('click', () => navBtnListener(contact));
 
 const navBtnListener = (domElement) => {
   if (main.firstChild !== domElement) {
