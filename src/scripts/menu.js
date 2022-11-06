197e7c1a4c1f7c85455eaa61abd728e1cffe9fe3
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 17:51:23 +1300

    Add all menu items into html generator

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -32,11 +32,23 @@ const generateMenuItems = (name, price) => {
 
   const startersUl = document.createElement('ul');
   startersUl.appendChild(generateMenuItems('Grass toast', 0.99));
+  startersUl.appendChild(generateMenuItems('Cheese toast', 3.99));
+  startersUl.appendChild(generateMenuItems('Marmite toast', 2.99));
+  startersUl.appendChild(generateMenuItems('Jalapenos and salami', 9.99));
+
+  const mainsUl = document.createElement('ul');
+  mainsUl.appendChild(generateMenuItems('Lamb rack', 19.99));
+  mainsUl.appendChild(generateMenuItems('Cumin spicy beetroot', 17.85));
+  mainsUl.appendChild(generateMenuItems('Tuna quiche', 99.05));
+  mainsUl.appendChild(generateMenuItems('Pomegranite sandwich', 44.99));
+  mainsUl.appendChild(generateMenuItems('Exquisite roast yams', 39.99));
+  mainsUl.appendChild(generateMenuItems('Blanched turkey', 19.07));
 
   menuDiv.appendChild(h1);
   menuDiv.appendChild(startersHeading);
   menuDiv.appendChild(startersUl);
   menuDiv.appendChild(mainsHeading);
+  menuDiv.appendChild(mainsUl);
 
   return menuDiv;
 })();
