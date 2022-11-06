c8d07605df33b0c169b192c0d96b28808a95ea57
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 18:08:51 +1300

    Seperate contact generation into another func

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,3 +1,18 @@
+const generateContactDetails = (description, details) => {
+  const li = document.createElement('li');
+
+  const descSpan = document.createElement('span');
+  descSpan.textContent = `${description}:`;
+
+  const detailSpan = document.createElement('span');
+  detailSpan.textContent = `${details}`;
+
+  li.appendChild(descSpan);
+  li.appendChild(detailSpan);
+
+  return li;
+};
+
 /**
  * Generates all html content for contact page, and returns it as an object
  */
@@ -11,19 +26,13 @@
 
   // Contact list
   const ul = document.createElement('ul');
-  
-  const address = document.createElement('li');
-  const addressSpan1 = document.createElement('span');
-  addressSpan1.textContent = 'Address';
-  const addressSpan2 = document.createElement('span');
-  addressSpan2.textContent = '324 Big Fisher Ave';
 
-  address.appendChild(addressSpan1);
-  address.appendChild(addressSpan2);
-
-  ul.appendChild(address);
+  ul.appendChild(generateContactDetails('Address', '324 Big Fisher Ave'));
+  ul.appendChild(generateContactDetails('Phone number', '01 234 5678'));
+  ul.appendChild(generateContactDetails('E-mail', 'example.one@two.com'));
 
   contactDiv.appendChild(h1);
   contactDiv.appendChild(ul);
+  
   return contactDiv;
 })();
