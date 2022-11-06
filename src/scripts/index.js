ac4698c900741ddbd318948e2ad12542d12f25f3
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 16:28:39 +1300

    Create 'main' element and append to div#content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,7 @@
 import header from './header.js'
 import '../styles/style.css';
 
+const main = document.createElement('main');
+
 document.querySelector('div#content').appendChild(header);
+document.querySelector('div#content').appendChild(main);
