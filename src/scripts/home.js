b5eb3fa4066a178437df774b55155145179afe9d
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 16:34:46 +1300

    Create first 'home' div and append to 'main' div

---
+++ src/scripts/home.js
@@ -0,0 +1,9 @@
+/**
+ * Generates all html for home page, and returns it as an object
+ */
+export default (() => {
+  const homeDiv = document.createElement('div');
+  homeDiv.setAttribute('id', 'home');
+
+  return homeDiv
+})();
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import header from './header.js'
+import home from './home.js';
 import '../styles/style.css';
 
 const main = document.createElement('main');
+main.appendChild(home);
 
 document.querySelector('div#content').appendChild(header);
 document.querySelector('div#content').appendChild(main);
