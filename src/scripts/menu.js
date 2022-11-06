ffce1b51fc800e5fbc4a9141e9b1318fd17e2543
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 17:47:19 +1300

    Create function ot gen menu items html

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,10 +1,26 @@
+const generateMenuItems = (name, price) => {
+  const li = document.createElement('li');
+
+  const nameSpan = document.createElement('span');
+  nameSpan.textContent = name;
+
+  const priceSpan = document.createElement('span');
+  priceSpan.textContent = `$${price}`;
+
+  li.appendChild(nameSpan);
+  li.appendChild(priceSpan);
+
+  return li;
+};
+
 /**
  * Generates all html content for menu page, and returns it as an object
  */
  export default (() => {
   const menuDiv = document.createElement('div');
   menuDiv.setAttribute('id', 'menu');
-  
+
+  // Headings
   const h1 = document.createElement('h1');
   h1.textContent = 'Menu';
 
@@ -14,8 +30,13 @@
   const mainsHeading = document.createElement('h2');
   mainsHeading.textContent = 'Mains';
 
+  const startersUl = document.createElement('ul');
+  startersUl.appendChild(generateMenuItems('Grass toast', 0.99));
+
   menuDiv.appendChild(h1);
   menuDiv.appendChild(startersHeading);
+  menuDiv.appendChild(startersUl);
   menuDiv.appendChild(mainsHeading);
+
   return menuDiv;
 })();
