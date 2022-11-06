3f9295660c88a7e91c7149c4b54ae24f9e70ceeb
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 17:19:25 +1300

    Add nav listener logic for tab navigation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,35 +1,26 @@
 import header from './header.js'
 import home from './home.js';
+import menu from './menu.js';
 import '../styles/style.css';
 
 const main = document.createElement('main');
-main.appendChild(home);
+main.appendChild(menu);
 
 document.querySelector('div#content').appendChild(header);
 document.querySelector('div#content').appendChild(main);
 
 document.querySelector('button[data-page="home"]')
-        .addEventListener('click', () => {
-          if (main.firstChild === home) {
-            return;
-          }
-
-          clearMain();
-          main.appendChild(home);
-  });
+        .addEventListener('click', () => navBtnListener(home));
 
 document.querySelector('button[data-page="menu"]')
-        .addEventListener('click', () => {
-          clearMain();
-  });
+        .addEventListener('click', () => navBtnListener(menu));
 
 document.querySelector('button[data-page="contact"]')
-        .addEventListener('click', () => {
-          clearMain();
-  });
+        .addEventListener('click', () => navBtnListener(document.createElement('div')));
 
-const clearMain = () => {
-  while (main.firstChild) {
+const navBtnListener = (domElement) => {
+  if (main.firstChild !== domElement) {
     main.removeChild(main.firstChild);
+    main.appendChild(domElement);
   }
-}
+};
---
+++ src/scripts/menu.js
@@ -0,0 +1,8 @@
+/**
+ * Generates all html content for menu page, and returns it as an object
+ */
+ export default (() => {
+  const menuDiv = document.createElement('div');
+
+  return menuDiv;
+})();
