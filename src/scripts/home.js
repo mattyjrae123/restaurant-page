f446999204e6065ccecbddc4b2bd1e9381e8c1e4
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 16:36:18 +1300

    Create h1 with JS and append to 'main' div

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -5,5 +5,10 @@ export default (() => {
   const homeDiv = document.createElement('div');
   homeDiv.setAttribute('id', 'home');
 
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Burger Shop';
+
+  homeDiv.appendChild(h1);
+
   return homeDiv
 })();
