31ab8102c0432d4bd4301cfd7314aba9117c129f
Matthew Rae <mattyjrae123@hotmail.com>
2022-11-06 17:39:16 +1300

    Generate menu headings with JS

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,6 +3,19 @@
  */
  export default (() => {
   const menuDiv = document.createElement('div');
+  menuDiv.setAttribute('id', 'menu');
+  
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Menu';
 
+  const startersHeading = document.createElement('h2');
+  startersHeading.textContent = 'Starters';
+
+  const mainsHeading = document.createElement('h2');
+  mainsHeading.textContent = 'Mains';
+
+  menuDiv.appendChild(h1);
+  menuDiv.appendChild(startersHeading);
+  menuDiv.appendChild(mainsHeading);
   return menuDiv;
 })();
